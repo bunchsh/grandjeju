85c27728ad3000f8e706e13f86da31b42569b92a
yang <ysw7939@gmail.com>
2022-01-17 17:06:24 +0900

    side menu 완성

---
+++ user/GJ1_main_page/assets/js/swiper.js
@@ -0,0 +1,14 @@
+/**
+ * @filename    : script.js
+ * @author      : 양수원 (ysw7939@gmail.com) 
+ * @description : swiper라이브러리 사용
+ */
+
+// swiper라이브러리 사용
+var swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
