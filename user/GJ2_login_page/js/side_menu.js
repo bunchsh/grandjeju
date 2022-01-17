57729c263b466172427498438d731b22b1add435
bunchsh <onee.ssong@gmail.com>
2022-01-17 17:49:39 +0900

    사이드바 스크롤 추가

---
+++ user/GJ2_login_page/js/side_menu.js
@@ -0,0 +1,40 @@
+/**
+ * @filename    : script.js
+ * @author      : 양수원 (ysw7939@gmail.com) 
+ * @description : 햄버거 사이드 메뉴바 작동코드
+ */
+
+// 햄버거 버튼
+const burger = document.querySelector(".hamburger_icon");
+const side = document.querySelector("#side_menu_bar");
+const x = document.querySelector(".x_button");
+
+burger.addEventListener("click" ,e =>{
+    side.classList.add("side_menu_bar_active")
+    side.style.transition = "transform 0.5s";
+})
+
+x.addEventListener("click" ,e =>{
+    side.classList.remove("side_menu_bar_active");
+})
+
+
+// 드롭다운
+const articles = document.querySelectorAll(".clear");
+const menu = document.querySelectorAll(".dropdown_menu")
+
+for(const article of articles){
+    article.addEventListener("click" ,e =>{
+        const sibling = article.nextElementSibling;
+        const img = article.lastElementChild;
+        
+        if (sibling.hasAttribute("id","dropdown_menu_active")){
+            sibling.removeAttribute("id","dropdown_menu_active");
+            sibling.style.transition = "transform 0.5s";
+            img.src = "./assets/img/down-arrow.png"
+        } else {
+            sibling.setAttribute("id","dropdown_menu_active");
+            img.src = "./assets/img/up-arrow.png"
+        }
+    })
+}
