ff2a68e96e605c2f64b63bca988bac4bb4febf8a
bunchsh <onee.ssong@gmail.com>
2022-01-18 18:21:41 +0900

    1:1 문의 상세 페이지 커밋

--- user/GJ7_reservation_detail_page/assets/js/cancel_alert_oneday.js
+++ user/GJ7_reservation_detail_page/assets/js/cancel_alert_oneday.js
@@ -1,5 +1,5 @@
 /**
- * @filename    : cancel_alert.js
+ * @filename    : cancel_alert_oneday.js
  * @author      : 한송희 (onee.ssong@gmail.com)
  * @description : 예약 취소 버튼을 눌렀을 때의 이벤트, Alert 창이 뜨는 함수
  */
