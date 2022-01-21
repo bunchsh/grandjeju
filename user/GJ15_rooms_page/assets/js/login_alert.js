1390e5407cf6c5e10f33a9dffdee82020f7ec341
bunchsh <onee.ssong@gmail.com>
2022-01-21 18:12:34 +0900

    room 상세 페이지 추가

---
+++ user/GJ15_rooms_page/assets/js/login_alert.js
@@ -0,0 +1,15 @@
+/**
+ * @filename    : login_alert.js
+ * @author      : 한송희 (onee.ssong@gmail.com)
+ * @description : 비로그인 상태에서 예약하기 버튼을 눌렀을 때 나타나는 이벤트
+ */
+
+ /** 로그인 Alert */
+ document.getElementsByClassName("btn_reservation_logout")[0].addEventListener('click', function () {
+    swal({
+        text: "로그인 후, 이용해 주세요.", // Alert 내용
+        buttons: {
+            confirm: "OK",  // 확인 버튼
+        }
+    })
+});
