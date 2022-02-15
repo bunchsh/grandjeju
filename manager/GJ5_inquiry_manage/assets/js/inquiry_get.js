9b9cd60a8d11034e4a7212923f6dd0cc841ab71a
yang <ysw7939@gmail.com>
2022-02-15 22:17:59 +0900

    문의관리자 수정페이지 중간 저장

---
+++ manager/GJ5_inquiry_manage/assets/js/inquiry_get.js
@@ -0,0 +1,55 @@
+/**
+ * @filename    : reservation_get.js
+ * @author      : 한송희 (onee.ssong@gmail.com) 
+ * @description : get 파라미터로 받을 예약 번호 가져오기
+ */
+
+/** 전역변수로 사용할 예약 번호 (GET파라미터로 받아야 함) */
+let reserv_id = null; // 아래에서 받은 reserv_id을 받아 사용 가능
+
+(async () => {
+    /** 입력을 위한 input 태그 객체 */
+    const inputType = document.querySelector("#type");
+    const inputTitle = document.querySelector("#title");
+    const inqueryEditor = document.querySelector("#inquiry_text");
+    const answerEditor = document.querySelector("#answer_text")
+
+    /** GET 파라미터 받기 */
+    const params = new URLSearchParams(window.location.search);
+    reserv_id = params.get('reserv_id');
+
+    // 파라미터가 정상적이지 않으므로 메시지 출력, 전페이지 이동 처리 후 수행 중단 (return)
+    if (!reserv_id) {
+        alert('예약 번호가 없습니다.');
+        history.back();
+        return;
+    }
+
+    /** 수정 form에 기존의 데이터를 표시하기 위해 Ajax로 저장된 데이터를 조회해야 함 */
+    let json = null;
+
+    try {
+        const response = await axios.get('/reservation/' + reserv_id);
+        json = response.data;
+    } catch (e) {
+        alert(e.response.data.rtmsg);
+        return;
+    }
+
+    // 조회 결과가 있다면 input 태그에 조회 데이터 세팅
+    if (json != null) {
+        console.log(json);
+        console.log(json.item[0].room);
+        roomPerson(json.item[0].room);
+
+        inputType.value = json.item[0].type;
+        inputTitle.value = json.item[0].title;
+        inputReservphone.value = json.item[0].reserv_phone;
+        inputRoom.value = json.item[0].room;
+        inputPerson.value = json.item[0].person
+        inputStaystart.value = json.item[0].stay_start;
+        inputStayend.value = json.item[0].stay_end;
+
+    }
+
+})();
