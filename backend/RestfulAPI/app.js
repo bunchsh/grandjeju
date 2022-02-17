a2c5d093e810d71d1f9e118424de8dde9438077d
bunchsh <onee.ssong@gmail.com>
2022-02-18 01:47:18 +0900

    app.js 라우팅했던 삭제된 파일 라우팅 삭제

--- backend/RestfulAPI/app.js
+++ backend/RestfulAPI/app.js
@@ -154,11 +154,6 @@ app.use(require('./Grandjeju/reservation')(app));
 app.use(require('./Grandjeju/review')(app));
 app.use(require('./Grandjeju/inquiry')(app));
 
-app.use(require('./Grandjeju/Members_test')(app));
-app.use(require('./Grandjeju/reservation_test')(app));
-app.use(require('./Grandjeju/review_test')(app));
-app.use(require('./Grandjeju/inquiry_test')(app));
-
 
 //런타임 에러가 발생한 경우에 대한 일괄 처리
 app.use((err, req, res, next) => {
