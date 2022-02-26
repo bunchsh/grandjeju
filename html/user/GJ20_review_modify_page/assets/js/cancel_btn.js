c66757486b5ccec274c9e34757a3fd5fef9878b2
yangsuwon <ysw7939@gmail.com>
2022-02-26 16:39:09 +0900

    리뷰 수정 삭제 버튼 구현 #30

---
+++ html/user/GJ20_review_modify_page/assets/js/cancel_btn.js
@@ -0,0 +1,24 @@
+document.querySelector('.btn_delete').addEventListener('click', e => {
+    swal({
+        text: "정말 삭제하시겠습니까?", // Alert 내용
+        buttons: {
+            OK : true,  // 확인 버튼
+            cancel: "cancel"    // 취소 버튼
+        }
+    }).then( async (value) => {   // 확인 버튼 이벤트
+        console.log(value);
+        if(value == 'OK'){
+            try {
+                const url = '/review/' + review_id;
+                await axios.delete(url);
+            } catch (e) {
+                // 에러가 발생한 경우 벡엔드가 주는 json 받기
+                
+                alert(e);
+                return;
+            }
+            window.location ="/GJ4_my_page/my.html"
+        }
+        
+    });
+})
