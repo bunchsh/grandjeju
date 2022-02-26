5a19ca8f604e8bda1d1c5c70e567ad483388d598
bunchsh <onee.ssong@gmail.com>
2022-02-26 21:28:11 +0900

    예약 취소 (예약 내역 삭제 / 환불 전) #15

--- html/user/GJ7_reservation_detail_page/assets/js/cancel_alert.js
+++ html/user/GJ7_reservation_detail_page/assets/js/cancel_alert.js
@@ -4,15 +4,63 @@
  * @description : 예약 취소 버튼을 눌렀을 때의 이벤트, Alert 창이 뜨는 함수
  */
 
+// const { DailyRotateFile } = require("winston/lib/winston/transports");
+
  /** 예약 취소 Alert */
- document.getElementsByClassName("btn_cancel")[0].addEventListener('click', function () {
+ 
+ document.getElementsByClassName("btn_cancel")[0].addEventListener('click', (e) => {
     swal({
         text: "예약을 취소하시겠습니까?", // Alert 내용
         buttons: {
             confirm: "OK",  // 확인 버튼
             cancel: true    // 취소 버튼
         }
-    }).then(function () {   // 확인 버튼 이벤트
-        location.href = "#";    // 이동
+    }).then(() => {   // 확인 버튼 이벤트
+
+        (async () => {
+            let json = null;
+
+            // 세션에 저장된 로그인 정보
+            try {
+                const response = await axios.get('/members/info');
+                json = response.data;
+            } catch (e) {
+                alert(e.response.data.rtmsg);
+                history.back();
+                return;
+            }
+
+            let member_id = json.item.member_id;
+
+            if (json != null) {
+                // 즉시실행 비동기 처리 함수
+                (async () => {
+                    e.preventDefault();
+
+                    const params = new URLSearchParams(window.location.search);
+                    const reserv_id = params.get('reserv_id');
+
+                    try {
+                        // Ajax 요청 보내기 -> 백엔드가 전달한 결과값이 response.data에 저장된다.
+                        const response = await axios.delete("/reservation/" + reserv_id);
+                        
+                        // 백엔드에서 전달된 결과가 로그인 성공을 의미하는 경우
+                        swal({
+                            text: "예약 취소가 완료되었습니다.", // Alert 내용
+                            buttons: {
+                                confirm: "OK",  // 확인 버튼
+                            }
+                        }).then(() => {
+                            window.location = "/GJ4_my_page/my.html"
+                        });
+                    } catch (error) {
+                        const errorMsg = "[" + error.response.status + "] " + error.response.statusText
+                        console.error(errorMsg);
+                        alert("예약 취소에 실패했습니다. 잠시 후 다시 시도해 주세요.");
+                    }
+                })();
+            };
+        })();
+        
     });
 });
