bad3b701c45a89abcf06a11f048648a109d0b826
bunchsh <onee.ssong@gmail.com>
2022-03-04 14:29:33 +0900

    하루 전 취소 불가 구현

--- html/user/GJ7_reservation_detail_page/assets/js/detail.js
+++ html/user/GJ7_reservation_detail_page/assets/js/detail.js
@@ -51,57 +51,22 @@
             const source = document.querySelector("#content-template").innerHTML;
             const template = Handlebars.compile(source);
             const html = template(json2.item[0]);
-
+            
             const now = dayjs();
-            const yesterday = now.subtract(1, "day").format("YYYY-MM-DD");
+            
+            const start_date = dayjs(json2.item[0].stay_start);
+            console.log(start_date);
+
+            const yesterday = start_date.subtract(1, "day").format("YYYY-MM-DD");
+            console.log(yesterday);
     
             document.querySelector('#content').insertAdjacentHTML('beforeend', html);
 
-             /** 예약 취소 Alert */
-            // document.getElementsByClassName("btn_cancel")[0].addEventListener('click', (e) => {
-            //     swal({
-            //         text: "예약을 취소하시겠습니까?", // Alert 내용
-            //         buttons: {
-            //             OK: true,  // 확인 버튼
-            //             cancel: "cancel"    // 취소 버튼
-            //         }
-            //     }).then(async (value) => {   // 확인 버튼 이벤트
-            //         if (value == 'OK') {
-            //             // 즉시실행 비동기 처리 함수
-            //         const params = new URLSearchParams(window.location.search);
-            //         const reserv_id = params.get('reserv_id');
-            //         console.log(reserv_id);
-
-            //         try {
-            //             const response = await axios.post("/payments/cancel", {
-            //                 reserv_id: reserv_id
-            //             });
-
-            //         } catch (error) {
-            //             const errorMsg = "[" + error.response.status + "] " + error.response.statusText
-            //             console.error(errorMsg);
-            //             alert("예약 취소에 실패했습니다. 잠시 후 다시 시도해 주세요.");
-            //         }
-            //         // Ajax 요청 보내기 -> 백엔드가 전달한 결과값이 response.data에 저장된다.
-            //         const response = await axios.delete("/reservation/" + reserv_id);
-            //         // 백엔드에서 전달된 결과가 로그인 성공을 의미하는 경우
-            //         swal({
-            //             text: "예약 취소가 완료되었습니다.", // Alert 내용
-            //             buttons: {
-            //                 confirm: "OK",  // 확인 버튼
-            //             }
-            //         }).then(() => {
-            //             window.location = "/GJ4_my_page/my.html"
-            //         });
-            //         }
-            //     });
-            // });
-
             /** 예약 취소 Alert */
             document.getElementsByClassName("btn_cancel")[0].addEventListener('click', (e) => {
-                if (now.format("YYYY-MM-DD") == json2.item[0].stay_start) {
+                if (now.format("YYYY-MM-DD") == json2.item[0].stay_start || now.format("YYYY-MM-DD") == yesterday) {
                     swal({
-                        text: "예약 취소가 불가합니다.",
+                        text: "예약 취소가 불가합니다. \n 예약 시 유의 사항을 확인해 주세요.",
                         button: {
                             OK: true
                         }
