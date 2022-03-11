b863f7236ddb6ac4891522d309a0af5979d21a3b
yangsuwon <ysw7939@gmail.com>
2022-03-11 23:07:21 +0900

    리뷰 작성 페이지 자바스크립트영역 파일 따로 생성

---
+++ html/user/GJ18_review_write_page/assets/js/review_write.js
@@ -0,0 +1,62 @@
+let login_info = null;
+(async () => {
+    try {
+        const response = await axios.get('/members/info');
+        login_info = response.data
+    } catch (e) {
+        swal({
+        text: e.response.data.rtmsg, // Alert 내용
+        buttons: {
+            OK: true,  // 확인 버튼
+        }
+    });
+        return;
+    }
+    console.log(login_info.item);
+})();
+document.querySelector('#review_write').addEventListener("submit", async(e) => {
+    e.preventDefault();
+
+    const regexHelper = new RegexHelper();
+
+    /** 제목 검사 */
+    if (!regexHelper.value("#title", "제목을 입력되지 않았습니다.")) {return false;}
+    /** 내용 검사 */
+    if (!regexHelper.editor_value(editor.getHTML(), "내용이 입력되지 않았습니다.")) {return false;}
+
+    const user_name = login_info.item.user_name;
+    const user_id = login_info.item.user_id;
+    const title = document.querySelector("#title").value;
+    const text = editor.getHTML();
+
+    let json = null;
+    let up = null;
+    try { 
+        const response = await axios.post("/review", {
+            user_name : user_name,
+            user_id : user_id,
+            title : title,
+            text : text
+        });
+        json = response.data;
+
+        id_array.forEach(async (v, i) => {
+            const updated = await axios.put("/photo/" + v ,{
+                review_id : json.item[0].review_id
+            });
+            up = updated.data;
+        })
+        
+    } catch (e) {
+        swal({
+        text: e.response.data.rtmsg, // Alert 내용
+        buttons: {
+            OK: true,  // 확인 버튼
+        }
+    });
+        return;
+    }
+    if(json != null) {
+        window.location ="/GJ19_review_datail_page/review_detail.html?review_id=" + json.item[0].review_id;
+    }
+})
